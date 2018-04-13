295cf54a15d92927b268a9d006c9cc470a472411
Sameer Khan <sameer_khan@student.uml.edu>
2018-04-13 01:32:39 -0400

    Subscriptions working now

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import withData from '../lib/apollo';
-import {graphql} from 'react-apollo';
+import {graphql, compose, Query, Subscription} from 'react-apollo';
 import gql from 'graphql-tag';
 
-const query = gql `
+import withData from '../lib/apollo';
+
+const ALL_USERS = gql `
   {
     allUsers {
       id
@@ -11,17 +12,52 @@ const query = gql `
   }
 `;
 
-const index = ({data: {
-    allUsers
-  }}) => (
-  <div>
-    Welcome to next.js!
-    <ul>
-      {allUsers.map(u => <li key={u.id}>{u.username}</li>)}
-    </ul>
-  </div>
-);
+const SUBSCRIPTIONS = gql `
+  subscription {
+    userAdded {
+      id
+      username
+    }
+  }
+`;
 
-const graphqlIndex = graphql(query)(index);
+class Index extends React.Component {
+  componentDidMount() {
+    this
+      .props
+      .data
+      .subscribeToMore({
+        document: SUBSCRIPTIONS,
+        updateQuery: (prev, {subscriptionData}) => {
+          if (!subscriptionData.data) {
+            return prev;
+          }
+          const newUser = subscriptionData.data.userAdded;
+          return {
+            ...prev,
+            allUsers: [
+              newUser, ...prev.allUsers
+            ]
+          }
+        }
+      })
+  }
+
+  render() {
+    const {
+      allUsers = []
+    } = this.props.data;
+
+    return (
+      <div>Welcome to next.js!
+        <ul>
+          {allUsers.map(u => <li key={u.id}>{u.username}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const graphqlIndex = compose(graphql(SUBSCRIPTIONS), graphql(ALL_USERS))(Index);
 
 export default withData(graphqlIndex);
